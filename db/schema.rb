c000b472f61ea2e5bdf7ff982dd1ceab6a7b2836
Sean Livingston <sean.d.livingston@gmail.com>
2014-03-07 07:23:01 -0500

    Using rails g migration to add fields to Events table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140301162705) do
+ActiveRecord::Schema.define(version: 20140307010832) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140301162705) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "starts_at"
+    t.text     "description"
   end
 
 end
